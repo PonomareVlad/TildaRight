26b5360946d0ceb5948560c769a13391a6fb6750
Vladislav Ponomarev <2877584+PonomareVlad@users.noreply.github.com>
2023-04-11 00:11:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /bot|google|baidu|bing|msn|teoma|slurp|yandex/i.test(navigator.userAgent) ||
 addEventListener('load', function () {
     setTimeout(function () {
-        if (document.getElementById('copyright'))
-            document.getElementById('copyright').style.display = 'none'
+        if (document.getElementById('tildacopy'))
+            document.getElementById('tildacopy').style.display = 'none'
     }, 100)
 })
